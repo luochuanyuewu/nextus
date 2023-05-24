e6a14fd5d4b627d7ea3152016f8237b18d589641
luochuanyuewu <luochuanyuewu@gmail.com>
2023-05-24 13:46:10 +0800

    update navibar

--- frontend/app/[lang]/components/Navbar.tsx
+++ frontend/app/[lang]/components/Navbar.tsx
@@ -2,9 +2,9 @@
 import Logo from "./Logo";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+// import classnames and useOnClickOutside
 import classNames from "classnames";
-import { title } from "process";
-import { AiOutlineLogin } from "react-icons/ai";
+import { useState } from "react";
 
 
 interface NavLink {
@@ -61,19 +61,27 @@ export default function Navbar({
   logoText: string | null;
 }) {
 
-  let router = useRouter()
-  const pathName = usePathname();
 
+  // add a state to toggle the dropdown
+  const [open, setOpen] = useState<boolean>();
+  //onclick handler when clicking a menu item
+
+  const handleClick = () => {
+    setOpen(false);
+  };
 
   return (
     <header >
       <div className="navbar bg-base-100 mx-auto max-w-7xl">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <div className={classNames({
+            dropdown: true,
+            'dropdown-open': open,
+          })}>
+            <label tabIndex={0} className="btn btn-ghost lg:hidden" onClick={() => setOpen((prev) => !prev)}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} onClick={handleClick} className={classNames({ 'menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52': true, hidden: !open })}>
               {links.map((item: NavLink) => (
                 <NavLink key={item.id} {...item} />
               ))}
