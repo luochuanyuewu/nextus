55e96055e1a34700156454c72e8a86ca2b28db7e
luochuanyuewu <luochuanyuewu@gmail.com>
2023-05-29 11:09:47 +0800

    fix

--- frontend/app/[lang]/page.tsx
+++ frontend/app/[lang]/page.tsx
@@ -1,6 +1,7 @@
+import LangRedirect from './components/LangRedirect';
 import { fetchAPI } from './utils/fetch-api';
 import { sectionRenderer } from './utils/section-renderer';
-
+import { i18n } from "i18n-config";
 async function getPageBySlug(slug: string, lang: string) {
     const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
@@ -13,6 +14,7 @@ async function getPageBySlug(slug: string, lang: string) {
 
 export default async function RootRoute({ params }: { params: { lang: string } }) {
     const page = await getPageBySlug('me', params.lang);
+    if (page.data.length == 0 && params.lang !== i18n.defaultLocale) return <LangRedirect />
     if (page.data.length === 0) return null;
     const contentSections = page.data[0].attributes.contentSections;
     return contentSections.map((section: any, index: number) => sectionRenderer(section, index));
