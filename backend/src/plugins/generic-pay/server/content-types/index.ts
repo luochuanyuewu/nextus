a5d2de6aa713bd902a0db32762390c63a1128e5c
luochuanyuewu <luochuanyuewu@gmail.com>
2023-06-18 21:33:26 +0800

    fix compile

--- backend/src/plugins/generic-pay/server/content-types/index.ts
+++ backend/src/plugins/generic-pay/server/content-types/index.ts
@@ -1,14 +1,4 @@
 'use strict';
 
-import { Schema } from '@strapi/strapi';
 
-interface SchemaObject {
-    [key: string]: {
-        schema: Schema;
-    };
-}
-
-const schemaObject: SchemaObject = {
-};
-
-export default schemaObject;
+export default {};
