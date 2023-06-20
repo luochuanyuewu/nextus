020d0c74be28f7b6f788b764265fc155ba7445e0
luochuanyuewu <luochuanyuewu@gmail.com>
2023-06-20 12:04:33 +0800

    update

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -10,4 +10,6 @@ export default ({ env }) => ({
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+  url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+  serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
 });
