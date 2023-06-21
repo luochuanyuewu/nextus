6c387eb6b0007cf2465bf7967d72ae3cd38609dd
luochuanyuewu <luochuanyuewu@gmail.com>
2023-06-21 14:21:07 +0800

    fix tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  // content: [
+  //   './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  //   './components/**/*.{js,ts,jsx,tsx,mdx}',
+  //   './app/**/*.{js,ts,jsx,tsx,mdx}',
+  // ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   darkMode: "class",
   theme: {
     extend: {
