30f01c66aa98d6b5079ad7e0ca2f20be8355d264
luochuanyuewu <luochuanyuewu@gmail.com>
2023-08-05 11:23:33 +0800

    chore: useScroll optimization

--- lib/hooks/useScroll.ts
+++ lib/hooks/useScroll.ts
@@ -1,24 +1,17 @@
 import { useState, useEffect } from 'react'
-import { useThrottleFn } from 'react-use' // react-use  sucks.
+// @ts-ignore
+import { default as throttle } from 'lodash.throttle'
 
 export default function useScroll() {
   const [progress, setProgress] = useState(0)
 
-  //   const updateProgress = useThrottle(() => {
-  //     const bodyHeight =
-  //       document.body.clientHeight - document.documentElement.clientHeight
-  //     const scrollPosition = typeof window !== 'undefined' ? window.scrollY : 0
-
-  //     setProgress(scrollPosition / bodyHeight)
-  //   }, 100)
-
-  const updateProgress = () => {
+  const handleScroll = throttle(() => {
     const bodyHeight =
       document.body.clientHeight - document.documentElement.clientHeight
-    const scrollPosition = typeof window !== 'undefined' ? window.scrollY : 0
+    const scrollPosition = window.scrollY
 
     setProgress(scrollPosition / bodyHeight)
-  }
+  }, 100)
 
   function scrollToTop() {
     if (typeof window !== 'undefined') {
@@ -30,11 +23,9 @@ export default function useScroll() {
   }
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', updateProgress)
-      return () => {
-        window.removeEventListener('scroll', updateProgress)
-      }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
     }
   }, [])
 
